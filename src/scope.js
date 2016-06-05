ac85241110ea8197e2bf3b7799a42456f6e987be
Khoa Tran <trandangkhoa@tma.com.vn>
2016-06-05 17:25:31 +0700

    Working on scope, add more test cases, digestOnce, digest

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,9 @@
 /* jshint globalstrict: true */
 'use strict';
 
+// Since js function equals nothing BUT ITSELF, we can create and use a dum function as an initial value.
+function initVal() {}
+
 function Scope() {
 
     this.$$watchers = [];
@@ -9,18 +12,33 @@ function Scope() {
 Scope.prototype.$watch = function(watchFn, listenerFn) {
     this.$$watchers.push({
         watchFn: watchFn,
-        listenerFn: listenerFn
+        listenerFn: listenerFn,
+        last: initVal
     });
 };
-Scope.prototype.$digest = function() {
+Scope.prototype.$$digestOnce = function() {
     var self = this;
-    var newValue, oldValue;
+    var newValue, oldValue, dirty;
     _.forEach(this.$$watchers, function(watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
         if (newValue !== oldValue) {
+            dirty = true;
             watcher.last = newValue;
-            watcher.listenerFn(newValue, oldValue, self);
+            if (typeof watcher.listenerFn === 'function') {
+                watcher.listenerFn(
+                    newValue,
+                    oldValue === initVal ? newValue : oldValue,
+                    self);
+            }
         }
     });
+    return dirty;
+};
+Scope.prototype.$digest = function() {
+    var dirty;
+    do {
+        dirty = this.$$digestOnce();
+    }
+    while (dirty);
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -70,6 +70,79 @@
             scope.$digest();
             expect(scope.counter).toBe(2);
         });
+
+        it('calls when watch value is at first "undefined"', function() {
+            scope.counter = 0;
+            scope.$watch(
+                function watcher() {
+                    return scope.someVal;
+                },
+                function listener() {
+                    scope.counter++;
+                }
+            );
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        });
+
+        it('calls listener with new value as old value the first time', function() {
+            scope.someVal = 123;
+            var oldValGiven;
+            scope.$watch(
+                function watcher() {
+                    return scope.someVal;
+                },
+                function listener(newVal, oldVal, scope) {
+                    oldValGiven = oldVal;
+                }
+            );
+
+            scope.$digest();
+            expect(oldValGiven).toBe(123);
+        });
+
+        it('may have watcher that does not come with listener funciton', function() {
+
+            var watchFn = jasmine.createSpy().and.returnValue('something');
+
+            scope.$watch(watchFn);
+
+            scope.$digest();
+
+            expect(watchFn).toHaveBeenCalled();
+        });
+
+        it('triggers chained watchers in the same digest', function() {
+            scope.name = 'Jane';
+
+            scope.$watch(
+                function watch() {
+                    return scope.nameUpper;
+                },
+                function(newVal, oldVal, scope) {
+                    if (newVal) {
+                        scope.initial = newVal.substring(0, 1) + '.';
+                    }
+                });
+
+            scope.$watch(
+                function watch() {
+                    return scope.name;
+                },
+                function(newVal, oldVal, scope) {
+                    if (newVal) {
+                        scope.nameUpper = newVal.toUpperCase();
+                    }
+                });
+
+            scope.$digest();
+            expect(scope.initial).toBe('J.');
+
+            scope.name = 'Bob';
+            scope.$digest();
+            expect(scope.initial).toBe('B.');
+        });
     });
 
 })();
