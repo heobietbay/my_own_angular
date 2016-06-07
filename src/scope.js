3b8c6c667210699d3be94e5177b89911e48cfe30
Khoa Tran <trandangkhoa@tma.com.vn>
2016-06-07 17:53:58 +0700

    1. Handle scenario value is NaN 2.Introduce  method

--- src/scope.js
+++ src/scope.js
@@ -21,6 +21,10 @@ Scope.prototype.$watch = function(watchFn, listenerFn, deepCompare) {
     });
 };
 
+Scope.prototype.$eval = function(expr, locals) {
+    return expr(this, locals);
+};
+
 Scope.prototype.$digest = function() {
     var dirty;
     var maxDigestIteration = 10;
@@ -43,7 +47,7 @@ Scope.prototype.$$digestOnce = function() {
     _.forEach(this.$$watchers, function(watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if ( !self.$$areEqual(newValue,oldValue,watcher.deepCompare) ) {
+        if (!self.$$areEqual(newValue, oldValue, watcher.deepCompare)) {
             dirty = true;
             self.$$lastDirtyWatch = watcher;
             watcher.last = (watcher.deepCompare ? _.cloneDeep(newValue) : newValue);
@@ -63,10 +67,12 @@ Scope.prototype.$$digestOnce = function() {
     return dirty;
 };
 
-Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
-    if (valueEq) {
+Scope.prototype.$$areEqual = function(newValue, oldValue, deepCompare) {
+    if (deepCompare) {
         return _.isEqual(newValue, oldValue);
     } else {
+        if (typeof newValue === 'number' && typeof oldValue === 'number' && isNaN(newValue) && isNaN(oldValue))
+            return true;
         return newValue === oldValue;
     }
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -231,12 +231,47 @@
             );
             scope.$digest();
             expect(scope.counter).toBe(1);
-            
+
             scope.aValue.push(4);
             scope.$digest();
             expect(scope.counter).toBe(2);
         });
 
+
+        it("correctly handle NaNs", function() {
+            scope.number = 0 / 0; // will be NaN;
+            scope.counter = 0;
+
+            scope.$watch(
+                function(scope) {
+                    return scope.number;
+                },
+                function(newValue, oldValue, scope) {
+                    scope.counter++;
+                }
+            );
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        });
+
+        it("executes $eval'ed function and returns result", function() {
+            scope.aValue = 42;
+            var result = scope.$eval(function(scope) {
+                return scope.aValue;
+            });
+            expect(result).toBe(42);
+        });
+
+        it("passes the second $eval argument straight through", function() {
+            scope.aValue = 42;
+            var result = scope.$eval(function(scope, arg) {
+                return scope.aValue + arg;
+            }, 2);
+            expect(result).toBe(44);
+        });
     });
 
 })();
